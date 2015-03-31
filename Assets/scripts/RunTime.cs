57c348b0ad91c031b28a2eb9026e3b6b7349250d
egroeg92 <grgmacrae@gmail.com>
2015-03-30 17:07:54 -0700

    projection metric bug fix + new metrics & meters

--- Assets/scripts/RunTime.cs
+++ Assets/scripts/RunTime.cs
@@ -25,8 +25,10 @@ public class RunTime : MonoBehaviour {
 	Vector3 presentPos;
 	Vector3 nextPos;
 
-	float dangerValue = 0f;
+	float dangerValueAngle = 0f;
+	float dangerValueDist = 0f;
 
+	int layerMask = 8;
 
 
 	PlayerTimeStamp currentNode;
@@ -146,7 +148,6 @@ public class RunTime : MonoBehaviour {
 		if (ReplayLast) {
 
 			if((Time.frameCount) % (replaySpeed) == 0){
-				Debug.Log (Time.frameCount + " "+ currentFrame);
 				currentFrame++;
 				if(currentFrame < playerNodes.Count )
 					currentNode = playerNodes[currentFrame];
@@ -183,20 +184,21 @@ public class RunTime : MonoBehaviour {
 		enemyMetricContainer emc = node.enemyMetricContainer;
 
 		//float actualDanger;
-		dangerValue = 0;
+		dangerValueAngle = 0;
+		dangerValueDist = 0;
 		if (emc.enemyMetrics.Count == 0)
-			dangerValue = 0;
+			dangerValueAngle = 0;
 		else {
-			dangerValue = 0;
+			dangerValueAngle = 0;
 			for (int i = 0; i < emc.enemyMetrics.Count ; i++) {
 				enemyMetric em = emc.enemyMetrics[i];
+				//only care if player can see the enemy
 				if(player.canSee(enemies[em.id])){
-					dangerValue += (calculateThreat(em.angle, em.distance)/(i+1));
+					dangerValueAngle += (calculateThreatEvenAngle(em.angle, em.distance)/(i+1));
+					dangerValueDist += (calculateThreatDistance(em.angle, em.distance)/(i+1));
+
 
 				}
-			
-
-				
 			}
 
 		}
@@ -207,11 +209,24 @@ public class RunTime : MonoBehaviour {
 			playerPathRenderer.SetVertexCount(currentFrame+1);
 			playerPathRenderer.SetPosition (currentFrame, node.pos);
 			
-			//draw seen danger
-			GameObject dangerMeter = GameObject.CreatePrimitive(PrimitiveType.Cube);
-			dangerMeter.renderer.material.color = Color.green;
-			dangerMeter.transform.localScale = new Vector3(.1f , dangerValue + .1f , .1f);
-			dangerMeter.transform.position = new Vector3(node.pos.x , map.transform.position.y + (dangerMeter.transform.localScale.y /2) , node.pos.z);
+			//draw danger
+			GameObject dangerMeterDist = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			//			Debug.Log (layerMask);
+			dangerMeterDist.layer = layerMask;
+			dangerMeterDist.renderer.material.color = Color.green;
+			dangerMeterDist.transform.localScale = new Vector3(.1f , dangerValueDist + .1f , .05f);
+			dangerMeterDist.transform.position = new Vector3(node.pos.x  , map.transform.position.y + (dangerMeterDist.transform.localScale.y /2) , node.pos.z- .025f);
+			
+
+
+			GameObject dangerMeterAngle = GameObject.CreatePrimitive(PrimitiveType.Cube);
+//			Debug.Log (layerMask);
+			dangerMeterAngle.layer = layerMask;
+			dangerMeterAngle.renderer.material.color = Color.cyan;
+			dangerMeterAngle.transform.localScale = new Vector3(.1f , dangerValueAngle + .1f , .05f);
+			dangerMeterAngle.transform.position = new Vector3(node.pos.x , map.transform.position.y + (dangerMeterAngle.transform.localScale.y /2) , node.pos.z+ .025f);
+
+
 
 
 
@@ -228,7 +243,7 @@ public class RunTime : MonoBehaviour {
 				
 				nextPos = presentPos + (dir * dist);
 
-				LineRenderer lr = dangerMeter.AddComponent<LineRenderer>();
+				LineRenderer lr = dangerMeterAngle.AddComponent<LineRenderer>();
 				lr.SetWidth(0.1F, 0.1F);
 				lr.material = new Material (Shader.Find("Particles/Additive"));
 				lr.SetColors(Color.blue, Color.blue);
@@ -236,65 +251,115 @@ public class RunTime : MonoBehaviour {
 				lr.SetPosition(0, presentPos);
 				lr.SetPosition(1, nextPos);
 
-				calculateProjectedThreat(currentFrame + pathPredictor , nextPos);
+				int projectedFrame = currentFrame + pathPredictor;
+				if(projectedFrame < playerNodes.Count)
+					calculateProjectedThreat(currentFrame + pathPredictor , nextPos);
 
 			}
 		}
 
 	}
 
-	float calculateThreat(float angle, float dist){
+	float calculateThreatEvenAngle(float angle, float dist){
 		float danger = 0;
 		float angleDanger = (angle/-180f) + 1 ;
 		float distDanger = player.losRange / (dist + 1); 
 		danger = angleDanger * distDanger;
+
 		return danger;
 	}
+
+	float calculateThreatDistance(float angle, float dist){
+		float danger = 0;
+
+		float angleDanger = (angle/-180f) + .1f ;
+
+		float distDanger = player.losRange / (dist + 1); 
+
+	//	Debug.Log ("angle " + angleDanger);
+	//	Debug.Log ("dist "+distDanger);
+
+		danger = angleDanger + distDanger;
+		
+		return danger;
+
+	}
+
+
+	//sort of hacky, created new player at projected spot and new enemies and calculated if it could see the enemies
 	void calculateProjectedThreat(int frame , Vector3 pos){
 
 		PlayerTimeStamp node = playerNodes[frame];
-		List<EnemyTimeStamp> enemies = node.enemies;
-		float danger = 0;
-		
-		Sprite s = Instantiate(player) as Sprite;
-		s.transform.forward = player.transform.forward;
-		s.transform.position = pos;
-		s.worldX = map.convertToWorldX(pos.x);
-		s.worldY = map.convertToWorldY(pos.z);
+		List<EnemyTimeStamp> enemyStamps = node.enemies;
 
-		Sprite en;
-		foreach(EnemyTimeStamp e in enemies){
-			en = Instantiate(player) as Sprite;
-			en.transform.forward = e.forward;
-			en.transform.position = e.pos;
-			en.worldX = (int)e.worldPos.x;
-			en.worldY = (int)e.worldPos.y;
+		Player p = Instantiate (player) as Player;
+		float worldX = map.convertToWorldX(pos.x);
+		float worldY = map.convertToWorldY(pos.z);
+		p.transform.position = pos;
+		float distDanger = 0;
+		float angleDanger = 0;
+
+		foreach(EnemyTimeStamp e in enemyStamps){
+			Vector3 eForward = e.forward;
+			Vector3 ePos = e.pos;
+			float eWorldX = e.worldPos.x;
+			float eWorldY = e.worldPos.y;
+
+			Enemy enemy = Instantiate(enemies[0]) as Enemy;
+			enemy.transform.rotation = e.rot;
+			enemy.transform.position = ePos;
+
+			
+			Vector3 to = pos - ePos;
+			Vector3 from = e.forward;
+
+			int lm = 1 << 8;
+			lm = ~lm;
+
+			RaycastHit hit;
+			Physics.Raycast(enemy.transform.position , to , out hit,Mathf.Infinity, lm);
+
+			if(hit.transform.gameObject == p.gameObject)
+			{
+				//enemyMetric em = new enemyMetric();
+
+				Vector2 eWorldPos = new Vector3(eWorldX, eWorldY);
+				Vector2 pWorldPos = new Vector2(worldX, worldY);
+						
+				float distance = (Vector2.Distance(eWorldPos, pWorldPos));
+				float angle = Vector3.Angle(to, from);
+
+				//Debug.Log (distance +" "+ angle);
 
 
-			if(s.canSee(en)){
-				enemyMetric em = new enemyMetric();
+				distDanger += calculateThreatDistance(angle, distance);
+				angleDanger += calculateThreatEvenAngle(angle, distance);
 
-				Vector2 ePos = e.worldPos;
-
-				Vector2 pPos = new Vector2(map.convertToWorldX(pos.x), map.convertToWorldY(pos.y));
-					
-				em.distance = (Vector2.Distance(ePos, pPos));
-				Vector3 to = pos - e.pos;
-				Vector3 from = e.forward;
-				em.angle = Vector3.Angle(to, from);
-
-				danger += calculateThreat(em.angle, em.distance);
 			}
-			Destroy(en.gameObject);
-			Destroy(en);
+
+			Destroy (enemy.gameObject);
+			Destroy(enemy);
 
 		}
-		Destroy (s.gameObject);
-		Destroy (s);
-		GameObject dangerMeter = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		dangerMeter.renderer.material.color = Color.red;
-		dangerMeter.transform.localScale = new Vector3(.1f , danger + .1f , .1f);
-		dangerMeter.transform.position = new Vector3(pos.x , map.transform.position.y + (dangerMeter.transform.localScale.y /2) , pos.z);
+
+		Destroy(p.gameObject);
+		Destroy (p);
+
+		GameObject dangerMeterDist = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		dangerMeterDist.layer = layerMask;
+		dangerMeterDist.renderer.material.color = Color.red;
+		dangerMeterDist.transform.localScale = new Vector3(.1f , distDanger + .1f , .05f);
+		dangerMeterDist.transform.position = new Vector3(pos.x , map.transform.position.y + (dangerMeterDist.transform.localScale.y /2) , pos.z-0.025f);
+
+		
+		GameObject dangerMeterAngle = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		dangerMeterAngle.layer = layerMask;
+		dangerMeterAngle.renderer.material.color = Color.magenta;
+		dangerMeterAngle.transform.localScale = new Vector3(.1f , angleDanger + .1f , .05f);
+		dangerMeterAngle.transform.position = new Vector3(pos.x , map.transform.position.y + (dangerMeterAngle.transform.localScale.y /2) , pos.z+0.025f);
+
+
+
 
 	}
 	PlayerTimeStamp createPlayerTimeStamp()
@@ -338,7 +403,7 @@ public class RunTime : MonoBehaviour {
 
 		if(ReplayLast)
 		{
-			dangerMeter.value = dangerValue;
+			dangerMeter.value = dangerValueAngle;
 
 		}
 
