f9abc1176d7e79b578a8ad409c31e4abceea89c6
egroeg92 <grgmacrae@gmail.com>
2015-03-31 14:49:15 -0700

    clean up

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -10,15 +10,13 @@ public class Enemy : Sprite {
 	public bool seesPlayer;
 	public FlashLight light;
 	Cell pos;
-	ArrayList visableCells;
-	
-	
-	
+
 	// Use this for initialization
 	void Start () {
 		base.Start ();
-		visableCells = new ArrayList ();
 		seesPlayer = false;
+
+		losRange = map.convertToWorldDistance (losRange);
 		
 	}
 	
@@ -30,40 +28,9 @@ public class Enemy : Sprite {
 		
 
 		float xmin,xmax;
-		visableCells.Clear (); 
-		for (int i = 0; i <= losRange; i++) {
-
-			xmax = (i * Mathf.Tan (Mathf.Deg2Rad * ( losAngle/2)));
-			xmin = -xmax;
-			for(int j = Mathf.RoundToInt(xmin) ; j <= xmax ; j++)
-			{
-				int x = j+worldX;
-				int y = i+worldY;
-
-				if(x >= map.gridX)
-					x = map.gridX-1;
-				if(y >= map.gridY)
-					y = map.gridY-1;
-				if(x<0)
-					x = 0;
-
-				//Debug.Log (x+","+y);
-				Cell c = map.grid[x,y];
-				visableCells.Add (c);
-
-
-			}
-				   
-			
-
-		}
-		visableCellsAtTime.Add (Time.frameCount, visableCells);
 	}
 
 
-	public ArrayList getVisibleCells(){
-		return visableCells;
-	}
 
 
 }
